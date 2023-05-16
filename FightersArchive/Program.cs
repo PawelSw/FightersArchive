933b825724c477414af179492de8de72fd60c545
PawelSw <swpawllo@gmail.com>
2023-05-16 16:10:03 +0200

    Generics

--- FightersArchive/Program.cs
+++ FightersArchive/Program.cs
@@ -5,7 +5,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var repository = new GenericRepository<Fighter, int>();
+        var repository = new GenericRepository<Fighter>();
         repository.Add(new Fighter { FirstName = "Mike", LastName = "Tyson", Wins = 50, Lost = 8, Weight = 105, Active = false });
         repository.Add(new Fighter {  FirstName = "Lennox", LastName = "Lewis", Wins = 44, Lost = 2, Weight = 115, Active = false });
         Fighter Holy = new Fighter { FirstName = "Evander", LastName = "Holyfield", Wins = 55, Lost = 12, Weight = 104, Active = false };
@@ -16,7 +16,7 @@ internal class Program
        // repository.Save();
 
 
-        var genericRepoWithRemove = new GenericRepositoryWithRemove<Fighter, int>();
+        var genericRepoWithRemove = new GenericRepositoryWithRemove<Fighter>();
         genericRepoWithRemove.Add(new Fighter { FirstName = "Tyson", LastName = "Fury", Wins = 33, Lost = 0, Weight = 125, Active = true });
         genericRepoWithRemove.Add(new Fighter { FirstName = "Deontay", LastName = "Wilder", Wins = 43, Lost = 3, Weight = 95, Active = true });
 
